eb423119fd0cf484bf0efb7d009cbc41854930f2
kostalev <arctic@list.ru>
2022-08-15 21:17:31 +0500

    Урок 6 Задачи 1 и 2 решение

---
+++ exmpl01/Program.cs
@@ -0,0 +1,125 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 1. Запрашиваем ввод произвольного количества чисел. (В условии не уточнен способ разделения и тип чисел. 
+//    Можно конечно отправить преподавателю запрос на уточнение тех. условий, но думаю не стоит. 
+//    Разделителем будет запятая, числа будут натуральные).
+// 2. Отбираем числа через отсечку по разделителям.
+// 3. Создаем массив. 
+// 3. Заполняем введенными числами.
+// 4. Проверяем количество чисел больше нуля.
+// 5. Выводим в консоль данные счетчика.
+
+
+void Print(int[] array) // Метод печати
+{
+    int size = array.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+string GetData(string title) //Метод считывания данных
+{
+    Console.Write(title);
+    return Console.ReadLine()!;
+}
+
+int CountSynbol(string s, char c) // Метод подсчета символов
+{
+    int count = 0;
+    int length = s.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (s[i] == c) count++;
+    }
+    return count;
+}
+
+int IndexOf(string s, char c) // Метод поиска (ищем позицию запятой, чтобы разделить строку на нужные числа)
+{
+
+    int length = s.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (s[i] == c)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+string Substring(string s, int position) //Метод выделения подстроки  с определенного элемента 
+{
+    string res = String.Empty;
+
+    int size = s.Length;
+    for (int i = position; i < size; i++)
+    {
+        res += $"{s[i]}";
+    }
+
+    return res;
+}
+
+string SubstringLength(string s, int position, int length) // Метод выделения искомого элемента из подстроки
+{
+    string res = String.Empty;
+
+    int size = s.Length;
+    for (int i = position; i < position + length; i++)
+    {
+        res += $"{s[i]}";
+    }
+
+    return res;
+}
+
+int[] Parse(string s) // Метод заполнения массива
+{
+    int[] result = new int[CountSynbol(s, ',') + 1];
+    s += ","; // Искусственно добавили, чтобы не получить зацикливание
+    int index = 0;
+
+    while (s.Length != 0)
+    {
+        int posSeparator = IndexOf(s, ',');
+        //System.Console.WriteLine($"pos {posSeparator}");
+        string need = SubstringLength(s, 0, posSeparator);
+        //System.Console.WriteLine($"need = {need}");
+        string o = Substring(s, posSeparator + 1);
+
+        //System.Console.WriteLine($"o    = {o}");
+        //Console.ReadLine();
+        s = o;
+        result[index] = Convert.ToInt32(need);
+        index++;
+    }
+
+    return result;
+}
+
+int GetCount(int[] array) // Метод подсчета чисел больше нуля
+{
+    int size = array.Length;
+    int count = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+string str = GetData("Введите числа через запятую: ");
+int[] array = Parse(str);
+Print(array);
+Console.WriteLine($"Пользователь ввел {GetCount(array)} чисел(ла) больше 0 ");
---
+++ exmpl02/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// Запрашиваем данные пользователя
+// Конвертируем в Double
+// Получаем x = (b2 - b1) / (k1 - k2), при k1 = k2 решений нет, при k1 = k2 и b2 = b1 совпадают
+// Получаем y = k1 * x + b1
+// Выводим в консоль координаты точки пересечения x, y
+
+
+double k1 = GetInput("Введите значение k1: ");
+double k2 = GetInput("Введите значение k2: ");
+double b1 = GetInput("Введите значение b1: ");
+double b2 = GetInput("Введите значение b2: ");
+
+
+double GetInput(string input) // метод преобразования в число
+{
+    Console.Write(input);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double ResultX(double k1, double k2, double b1, double b2) // метод расчета координаты Х точки пересечения
+{
+    double x = 0;
+
+    if ((k1 - k2) != 0)
+    {
+        x = (b2 - b1) / (k1 - k2);
+    }
+    return x;
+}
+
+double ResultY(double k1, double b1) // метод расчета координаты Y точки пересечения
+{
+    double y = 0;
+    if ((k1 - k2) != 0)
+    {
+        y = k1 * ResultX(k1, k2, b1, b2) + b1;
+    }
+    return y;
+}
+
+void Print(double b1, double b2, double k1, double k2) // метод печати
+{
+
+    double resultK = k1 - k2;
+    double resultB = b2 - b1;
+
+    if (resultK == 0 && resultB != 0)
+    {
+        Console.WriteLine("Решений нет. Прямые не пересекаются");
+    }
+    else if (resultK == 0 && resultB == 0)
+    {
+        Console.WriteLine($"Прямые совпадают");
+    }
+    else
+    {
+        double x = ResultX(k1, k2, b1, b2);
+        double y = ResultY(k1, b1);
+        Console.WriteLine($"Точка пересечения двух прямых имеет координаты X={x}, Y={y}");
+    }
+
+}
+
+Print(b1, b2, k1, k2);
+
